b1f262ae6ab77dfd14c99be742b515b9f1df9bbc
Sandeep Gill <sandeepgill62@gmail.com>
2023-07-13 04:56:32 +0000

    SQL Injection: SQL into two different parts (query string and values)

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,6 +7,28 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+    console.log("=============================================================");
+  }).catch(err => console.error('query error', err.stack));
+
+
 // pool.query(`
 // SELECT id, name, cohort_id
 // FROM students
@@ -47,25 +69,3 @@ const pool = new Pool({
 //     })
 //     console.log("=============================================================");
 //   }).catch(err => console.error('query error', err.stack));
-
-
-const queryString = `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `;
-
-const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
-// Store all potentially malicious values in an array.
-const values = [`%${cohortName}%`, limit];
-
-pool.query(queryString, values)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-    })
-    console.log("=============================================================");
-  }).catch(err => console.error('query error', err.stack));
